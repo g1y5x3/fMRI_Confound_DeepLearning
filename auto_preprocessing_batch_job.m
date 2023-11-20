579aaa0b0fff91e867469d6732d9732502a97a0e
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-11-20 13:48:19 -0600

    preprocessing

---
+++ auto_preprocessing_batch_job.m
@@ -0,0 +1,64 @@
+% FolderInfo = dir('/home/iris/yg5d6/Workspace/IXI_dataset/');
+load('filelist.mat')
+% 583
+for i = 3 : 583
+    filename = sprintf('%s/%s\n', FolderInfo(i).folder, FolderInfo(i).name);
+    
+    matlabbatch{1}.spm.spatial.preproc.channel.vols = {filename};
+    matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
+    matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
+    matlabbatch{1}.spm.spatial.preproc.channel.write = [0 1];
+    matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'/home/iris/yg5d6/Dropbox/Thesis/fMRI_confounding_removal/spm12/tpm/TPM.nii,1'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
+    matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {'/home/iris/yg5d6/Dropbox/Thesis/fMRI_confounding_removal/spm12/tpm/TPM.nii,2'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
+    matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {'/home/iris/yg5d6/Dropbox/Thesis/fMRI_confounding_removal/spm12/tpm/TPM.nii,3'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
+    matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {'/home/iris/yg5d6/Dropbox/Thesis/fMRI_confounding_removal/spm12/tpm/TPM.nii,4'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
+    matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [1 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {'/home/iris/yg5d6/Dropbox/Thesis/fMRI_confounding_removal/spm12/tpm/TPM.nii,5'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
+    matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [1 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {'/home/iris/yg5d6/Dropbox/Thesis/fMRI_confounding_removal/spm12/tpm/TPM.nii,6'};
+    matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
+    matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
+    matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
+    matlabbatch{1}.spm.spatial.preproc.warp.mrf = 1;
+    matlabbatch{1}.spm.spatial.preproc.warp.cleanup = 1;
+    matlabbatch{1}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+    matlabbatch{1}.spm.spatial.preproc.warp.affreg = 'mni';
+    matlabbatch{1}.spm.spatial.preproc.warp.fwhm = 0;
+    matlabbatch{1}.spm.spatial.preproc.warp.samp = 3;
+    matlabbatch{1}.spm.spatial.preproc.warp.write = [0 1];
+    matlabbatch{1}.spm.spatial.preproc.warp.vox = NaN;
+    matlabbatch{1}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                                  NaN NaN NaN];
+    matlabbatch{2}.spm.spatial.normalise.write.subj.def(1) = cfg_dep('Segment: Forward Deformations', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','fordef', '()',{':'}));
+    matlabbatch{2}.spm.spatial.normalise.write.subj.resample(1) = cfg_dep('Segment: c1 Images', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','tiss', '()',{1}, '.','c', '()',{':'}));
+    matlabbatch{2}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+                                                              78 76 85];
+    matlabbatch{2}.spm.spatial.normalise.write.woptions.vox = [2 2 2];
+    matlabbatch{2}.spm.spatial.normalise.write.woptions.interp = 4;
+    matlabbatch{2}.spm.spatial.normalise.write.woptions.prefix = 'w';
+    matlabbatch{3}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+    matlabbatch{3}.spm.spatial.smooth.fwhm = [10 10 10];
+    matlabbatch{3}.spm.spatial.smooth.dtype = 0;
+    matlabbatch{3}.spm.spatial.smooth.im = 0;
+    matlabbatch{3}.spm.spatial.smooth.prefix = 's';
+    
+    save preprocessing_batch matlabbatch % save the setup into a matfile called preprocessing_batch.mat
+    spm_jobman('run',matlabbatch) % execute the batch
+    clear matlabbatch % clear matlabbatch    
+    
+end
+
+
