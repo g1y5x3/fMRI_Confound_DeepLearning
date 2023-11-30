5e9cfec2fd66ce8a6b400db17b2bbc0b1e5507a1
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-11-30 16:00:48 -0600

    add MAE for age prediction

--- train.py
+++ train.py
@@ -1,38 +1,53 @@
+import os
+import argparse
 import wandb
 import torch
 import torch.optim as optim
+import torch.nn.functional as F
 from torch import nn, optim
 from torch.utils.data import DataLoader
+
 from torchinfo import summary
-from tqdm import tqdm
+from tqdm import trange
 from sfcn import SFCN
 from imageloader import IXIDataset
+
+WANDB = os.getenv("WANDB", False)
+
+parser = argparse.ArgumentParser(description="Example:")
+parser.add_argument("--bs",    type=int, default=8,  help="batch size")
+parser.add_argument("--nw",    type=int, default=6,  help="number of workers for the dataloader")
+parser.add_argument("--epoch", type=int, default=10, help="total number of epochs")
+args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # TODO: Test with other range that does not produce a x64 output
-bin_range   = [21,85] 
-batch_size  = 8
-num_workers = 6
-num_epochs  = 130
+bin_range   = [21,85]
+batch_size  = args.bs
+num_workers = args.nw
+num_epochs  = args.epoch
 lr = 1e-2
 wd = 1e-3
 
-wandb.login()
-run = wandb.init(
-  project="Confounding in fMRI Deep Learning",
-  config={
-    "num_workers":   num_workers,
-    "num_epochs":    num_epochs,
-    "learning_rate": lr,
-    "weight_decay":  wd,
-  },
-)
+if WANDB:
+  wandb.login()
+  run = wandb.init(
+    project="Confounding in fMRI Deep Learning",
+    config={
+      "num_workers":   num_workers,
+      "num_epochs":    num_epochs,
+      "learning_rate": lr,
+      "weight_decay":  wd,
+    },
+  )
 
-data_train = IXIDataset(data_dir="/home/iris/yg5d6/Workspace/IXI_dataset/preprocessed", label_file="IXI_train.csv", bin_range=bin_range)
-data_test  = IXIDataset(data_dir="/home/iris/yg5d6/Workspace/IXI_dataset/preprocessed", label_file="IXI_test.csv",  bin_range=bin_range)
+print("\nDataloader:")
+data_train = IXIDataset(data_dir="data", label_file="IXI_train.csv", bin_range=bin_range)
+data_test  = IXIDataset(data_dir="data", label_file="IXI_test.csv",  bin_range=bin_range)
 dataloader_train = DataLoader(data_train, batch_size=batch_size, num_workers=num_workers, shuffle=True)
 dataloader_test  = DataLoader(data_test,  batch_size=batch_size, num_workers=num_workers, shuffle=False)
+bin_center = data_train.bin_center.reshape([-1,1])
 
 x, y = next(iter(dataloader_train))
 print("\nTraining data summary:")
@@ -40,37 +55,43 @@ print(f"Total data: {len(data_train)}")
 print(f"Input {x.shape}")
 print(f"Label {y.shape}")
 
-x, y = next(iter(dataloader_test))
-print("\nTesting data summary:")
-print(f"Total data: {len(data_test)}")
-print(f"Input {x.shape}")
-print(f"Label {y.shape}")
-
+# x, y = next(iter(dataloader_test))
 # print("\nTesting data summary:")
+# print(f"Total data: {len(data_test)}")
+# print(f"Input {x.shape}")
+# print(f"Label {y.shape}")
 
 model = SFCN(output_dim=y.shape[1])
 print(f"\nModel Dtype: {next(model.parameters()).dtype}")
 summary(model, x.shape)
 model.to(device)
 
-# this loss expects the argument input in log-space, default would automatically
-# convert target into log space
-# https://pytorch.org/docs/stable/generated/torch.nn.KLDivLoss.html
 criterion = nn.KLDivLoss(reduction="batchmean", log_target=True)
+
 
 optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=wd)
 # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.3)
 
-print("\nTraining:")
-for epoch in tqdm(range(num_epochs)):
-  epoch_loss = 0.0
+t = trange(num_epochs, desc="\nTraining", leave=True)
+for epoch in t:
+  epoch_loss_kl = 0.0
+  epoch_MAE_age = 0.0
   for images, labels in dataloader_train:
-    x, y = images.to(device), labels.to(device)
     optimizer.zero_grad()
+    x, y = images.to(device), labels.to(device)
     output = model(x)
     loss = criterion(output.log(), y.log())
     loss.backward()
     optimizer.step()
-    epoch_loss += loss.item()
-  print(f"epoch={epoch}, loss={epoch_loss}")
-  wandb.log({"loss": epoch_loss})
+
+    with torch.no_grad():
+      age_target = labels @ bin_center
+      age_pred   = output.cpu() @ bin_center
+
+      epoch_MAE_age += F.l1_loss(age_pred, age_target, reduction="sum")
+      epoch_loss_kl += loss.item()
+
+  t.set_description(f"Training: train/loss_kl {epoch_loss_kl:.2f}, train/MAE_age {epoch_MAE_age/len(data_train):.2f}")
+  if WANDB:
+    wandb.log({"train/loss_kl": epoch_loss_kl,
+               "train/MAE_age": epoch_MAE_age/len(data_train)})
