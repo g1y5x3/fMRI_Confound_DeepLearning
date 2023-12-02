10fb24eb5ac28d38d3adfc0ad3d1aa7f05b83b7f
Yixiang Gao <yg5d6@umsystem.edu>
2023-12-02 04:54:10 +0000

    add a few more hyper-param to tune

--- train.py
+++ train.py
@@ -15,9 +15,13 @@ from imageloader import IXIDataset
 WANDB = os.getenv("WANDB", False)
 
 parser = argparse.ArgumentParser(description="Example:")
-parser.add_argument("--bs",    type=int, default=8,  help="batch size")
-parser.add_argument("--nw",    type=int, default=6,  help="number of workers for the dataloader")
-parser.add_argument("--epoch", type=int, default=10, help="total number of epochs")
+parser.add_argument("--bs",    type=int,   default=8,    help="batch size")
+parser.add_argument("--nw",    type=int,   default=6,    help="number of workers for the dataloader")
+parser.add_argument("--epoch", type=int,   default=10,   help="total number of epochs")
+parser.add_argument("--lr",    type=float, default=1e-2, help="learning rate")
+parser.add_argument("--wd",    type=float, default=1e-3, help="weight decay")
+parser.add_argument("--step",  type=int,   default=30,   help="step size")
+parser.add_argument("--gamma", type=float, default=0.3,  help="gamma")
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,18 +32,23 @@ bin_range   = [21,85]
 batch_size  = args.bs
 num_workers = args.nw
 num_epochs  = args.epoch
-lr = 1e-2
-wd = 1e-3
+lr          = args.lr
+wd          = args.wd
+step        = args.step
+gamma       = args.gamma
 
 if WANDB:
   wandb.login()
   run = wandb.init(
     project="Confounding in fMRI Deep Learning",
     config={
+      "batch_size":    batch_size,
       "num_workers":   num_workers,
       "num_epochs":    num_epochs,
       "learning_rate": lr,
       "weight_decay":  wd,
+      "step_size":     step,
+      "gamma":         gamma,
     },
   )
 
@@ -49,6 +58,7 @@ data_test  = IXIDataset(data_dir="data", label_file="IXI_test.csv",  bin_range=b
 dataloader_train = DataLoader(data_train, batch_size=batch_size, num_workers=num_workers, pin_memory=True, shuffle=True)
 dataloader_test  = DataLoader(data_test,  batch_size=batch_size, num_workers=num_workers, pin_memory=True, shuffle=False)
 bin_center = data_train.bin_center.reshape([-1,1])
+bin_center = bin_center.to(device)
 
 x, y = next(iter(dataloader_train))
 print("\nTraining data summary:")
@@ -70,7 +80,7 @@ model.to(device)
 
 criterion = nn.KLDivLoss(reduction="batchmean", log_target=True)
 optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=wd)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.3)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=step, gamma=gamma)
 
 t = trange(num_epochs, desc="\nTraining", leave=True)
 for epoch in t:
@@ -85,8 +95,8 @@ for epoch in t:
     optimizer.step()
 
     with torch.no_grad():
-      age_target = labels @ bin_center
-      age_pred   = output.cpu() @ bin_center
+      age_target = y @ bin_center
+      age_pred   = output @ bin_center
       MAE_age = F.l1_loss(age_pred, age_target, reduction="mean")
 
     loss_kl_train += loss.item()
@@ -100,11 +110,11 @@ for epoch in t:
     MAE_age_test = 0.0
     for images, labels in dataloader_test:
       x, y = images.to(device), labels.to(device)
-      output = model(images.to(device))
+      output = model(x)
       loss = criterion(output.log(), y.log())
 
-      age_target = labels @ bin_center
-      age_pred = output.cpu() @ bin_center
+      age_target = y @ bin_center
+      age_pred   = output @ bin_center
       MAE_age = F.l1_loss(age_pred, age_target, reduction="mean")
 
       loss_kl_test += loss.item()
@@ -124,9 +134,9 @@ for epoch in t:
                })
 
 torch.save(model.state_dict(), "model/model.pth")
-
 if WANDB:
   artifact = wandb.Artifact("model", type="model")
   artifact.add_file("model/model.pth")
   run.log_artifact(artifact)
   run.finish()
+
