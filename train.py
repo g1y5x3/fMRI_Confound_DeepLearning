9d3cec1e3d61b7bc28579b6eedcacc0765deb043
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-12-03 14:25:40 -0600

    better function argument

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import os
-import yaml
 import wandb
 import argparse
 import torch
@@ -15,7 +14,7 @@ from imageloader import IXIDataset
 
 WANDB = os.getenv("WANDB", False)
 
-def train(config, log=None, run=None):
+def train(config, run=None):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
   print(config) 
@@ -97,14 +96,14 @@ def train(config, log=None, run=None):
     scheduler.step()
   
     t.set_description(f"Training: train/loss_kl {loss_kl_train:.2f}, train/MAE_age {MAE_age_train:.2f} test/loss_kl {loss_kl_test:.2f}, test/MAE_age {MAE_age_test:.2f}")
-    if log:
+    if run:
       wandb.log({"train/loss_kl": loss_kl_train,
                  "train/MAE_age": MAE_age_train,
                  "test/loss_kl":  loss_kl_test,
                  "test/MAE_age":  MAE_age_test,
                  })
   
-  if log:
+  if run:
     torch.save(model.state_dict(), "model/model.pth")
     artifact = wandb.Artifact("model", type="model")
     artifact.add_file("model/model.pth")
@@ -126,16 +125,13 @@ if __name__ == "__main__":
   args = parser.parse_args()
   config = vars(args)
 
-  log = None
   run = None
   if WANDB:
-    log = 1
     # TODO need to pass project/group without using argparse
     run = wandb.init(
       project = "Confounding-in-fMRI-Deep-Learning-Test",
-      group   = "Sweep-Debug",
       config  = config
     )
   
-  train(config, log, run)
+  train(config, run)
   
--- train_sweep.py
+++ train_sweep.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
   
   run = wandb.init(config = config)
   
-  train(wandb.config, 1, run)
+  train(wandb.config, run)
   
