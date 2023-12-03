5750060832f7bedcbb5288b55c876e7fa4ba7824
Yixiang Gao <yg5d6@umsystem.edu>
2023-12-03 19:03:35 +0000

    add sweep configuration

---
+++ train_sweep.py
@@ -0,0 +1,16 @@
+import os
+import yaml
+import wandb
+from train import train
+
+os.environ["WANDB"] = "1"
+
+if __name__ == "__main__":
+
+  with open("config.yaml") as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+  
+  run = wandb.init(config = config)
+  
+  train(wandb.config, 1, run)
+  
